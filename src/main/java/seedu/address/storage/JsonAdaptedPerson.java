8a954456184cd4447a917f512fc199b576048ba5
reineoeq <e1354688@u.nus.edu>
2025-10-10 01:25:02 +0800

    Checkstyle fixes

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -82,7 +82,9 @@ class JsonAdaptedPerson {
         }
 
         if (studentId == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, StudentId.class.getSimpleName()));
+            throw new IllegalValueException(
+                String.format(MISSING_FIELD_MESSAGE_FORMAT, StudentId.class.getSimpleName())
+            );
         }
         if (!StudentId.isValidStudentId(studentId)) {
             throw new IllegalValueException(StudentId.MESSAGE_CONSTRAINTS);
