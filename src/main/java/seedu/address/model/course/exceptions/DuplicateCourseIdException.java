3c8bf86514f9356d89d31da8746ba050cdb92181
gnahuy123 <e1398507@u.nus.edu>
2025-10-09 23:05:21 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/course/exceptions/DuplicateCourseIdException.java
+++ src/main/java/seedu/address/model/course/exceptions/DuplicateCourseIdException.java
@@ -1,5 +1,8 @@
 package seedu.address.model.course.exceptions;
 
+/**
+ * Signals that there is a duplicate course ID.
+ */
 public class DuplicateCourseIdException extends RuntimeException {
     public DuplicateCourseIdException() {
         super("CourseId already exists");
