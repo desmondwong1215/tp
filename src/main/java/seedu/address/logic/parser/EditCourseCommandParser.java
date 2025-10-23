6b2d7669562fb777613e25ab476ef78f91e842c1
desmondwong1215 <desmondwong1215@gmail.com>
2025-10-23 22:27:41 +0800

    Remove unnecessary function

--- src/main/java/seedu/address/logic/parser/EditCourseCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCourseCommandParser.java
@@ -7,10 +7,6 @@ import static seedu.address.logic.commands.EditCourseCommand.MESSAGE_USAGE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
 import java.util.logging.Logger;
 
 import seedu.address.commons.core.LogsCenter;
@@ -19,7 +15,6 @@ import seedu.address.logic.commands.EditCourseCommand;
 import seedu.address.logic.commands.EditCourseCommand.EditCourseDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ModelManager;
-import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new EditCourseCommand object
@@ -64,20 +59,5 @@ public class EditCourseCommandParser implements Parser<EditCourseCommand> {
         }
 
         return new EditCourseCommand(index, editCourseDescriptor);
-    }
-
-    /**
-     * Parses {@code Collection<String> tags} into a {@code Set<Tag>} if {@code tags} is non-empty.
-     * If {@code tags} contain only one element which is an empty string, it will be parsed into a
-     * {@code Set<Tag>} containing zero tags.
-     */
-    private Optional<Set<Tag>> parseTagsForEdit(Collection<String> tags) throws ParseException {
-        assert tags != null;
-
-        if (tags.isEmpty()) {
-            return Optional.empty();
-        }
-        Collection<String> tagSet = tags.size() == 1 && tags.contains("") ? Collections.emptySet() : tags;
-        return Optional.of(ParserUtil.parseTags(tagSet));
     }
 }
