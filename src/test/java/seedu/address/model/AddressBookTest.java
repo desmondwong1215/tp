a146991ba7dab57c3f56efce4586bf1a78f58a61
Beastarz <jygoh1453@gmail.com>
2025-10-28 17:59:20 +0800

    fix checkstyle

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -10,7 +10,6 @@ import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
