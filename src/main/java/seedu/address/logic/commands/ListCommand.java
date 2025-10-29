65367ff9ad40d72f8fa986fb1926fa84613aa154
gnahuy123 <e1398507@u.nus.edu>
2025-10-29 18:53:06 +0800

    Clean checkstyle

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_COURSES;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
 
